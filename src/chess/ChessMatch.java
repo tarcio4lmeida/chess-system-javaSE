ca2040ed028f00869c99fde1ca3e52dcc132c597
Tárcio Almeida <tarcioalmeidalima@gmail.com>
2020-02-18 01:45:41 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -84,7 +84,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.PlacePiece(p, target);
 		if (capturedPiece!=null) {
@@ -96,7 +97,8 @@ public class ChessMatch {
 		
 	}
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece) board.removePiece(target);
+		p.decreaseMoveCount();
 		board.PlacePiece(p, source);
 		
 		if (capturedPiece!=null) {
