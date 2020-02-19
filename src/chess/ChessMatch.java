5bd744b249e1dcfa4f4f629f4972866e22942aef
Tárcio Almeida <tarcioalmeidalima@gmail.com>
2020-02-19 00:23:29 -0300

    Queen and pawn bug fix

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -191,6 +192,7 @@ public class ChessMatch {
 			placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 	        placeNewPiece('e', 1, new King(board, Color.WHITE));
 	        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+	        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 	        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
@@ -207,6 +209,7 @@ public class ChessMatch {
 			placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 
 	        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+	        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 	        placeNewPiece('e', 8, new King(board, Color.BLACK));
 	        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 	        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
